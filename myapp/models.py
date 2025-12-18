c8755c43d8eaabfccde8a1180816361ddac59dae
Rudra Pratap Singh <rudra290806@gmail.com>
2025-12-18 17:36:47 +0530

    Basic login page created and built in django User-model to be used

--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-      username=models.CharField(max_length=30)
-      password=models.CharField(max_length=30)
+
+
+class Profile(models.Model):
+      user=models.OneToOneField(User,on_delete=models.CASCADE)
+
       Admin_Right=models.BooleanField(default=False)
       oauth_User_Id=models.CharField(max_length=50)
-      email=models.EmailField()
+#       email=models.EmailField()
       profile_picture = models.ImageField(upload_to='profile_pics/')
       bio=models.TextField(max_length=300)
       batch=models.CharField(max_length=10)
@@ -14,7 +17,7 @@ class User(models.Model):
       no_of_downloads=models.IntegerField(default=0)
       joined_at=models.DateTimeField(auto_now_add=True)
 def __str__(self):
-        return self.username
+        return self.user.username
 class  Location(models.Model):
     name=models.CharField(max_length=50)
 
